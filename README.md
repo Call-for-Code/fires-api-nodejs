f5c5a0d0baf0615f01fe3877e51bb0667a14a851
Raj Singh <rrsingh@us.ibm.com>
2018-08-30 15:57:54 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# fires-api-nodejs
-Access global daily fire reports and store in Cloudant as GeoJSON
+# NASA Active Fire Data API access for IBM Cloud
+
+This project shows how to build a basic data access application that continuously runs in the background, processing VIIRS (I-Band 375 m) data from NASA by dowloading CSV files from the site, transforming each record into GeoJSON, and loading that into a Cloudant database.
+
+From [NASA Firms](https://earthdata.nasa.gov/earth-observation-data/near-real-time/firms/viirs-i-band-active-fire-data): 
+> The Visible Infrared Imaging Radiometer Suite (VIIRS) 375 m ([VNP14IMGTDL_NRT](https://earthdata.nasa.gov/earth-observation-data/near-real-time/firms/v1-vnp14imgt)) active fire product is the latest product to be added to FIRMS. It provides data from the VIIRS sensor aboard the joint NASA/NOAA Suomi National Polar-orbiting Partnership (Suomi-NPP) satellite. The 375 m data complements Moderate Resolution Imaging Spectroradiometer (MODIS) fire detection; they both show good agreement in hotspot detection but the improved spatial resolution of the 375 m data provides a greater response over fires of relatively small areas and provides improved mapping of large fire perimeters. The 375 m data also has improved nighttime performance. Consequently, these data are well suited for use in support of fire management (e.g., near real-time alert systems), as well as other science applications requiring improved fire mapping fidelity. Recommended reading: [VIIRS 375 m Active Fire Algorithm User Guide](https://viirsland.gsfc.nasa.gov/PDF/VIIRS_activefire_User_Guide.pdf) (updated July 2018).
+
+## Get an IBM Cloud account
+
+If you haven't yet done it, sign up for [IBM Cloud here](https://console.bluemix.net/).
+
+## Getting Started in IBM Cloud
+
+Deploy this application to IBM Cloud.
+
+1. Install and configure the [IBM Cloud Developer Tools](https://console.bluemix.net/docs/cli/index.html#overview)
+2. Clone this repository
+   ```
+   $ git clone git@github.com:rajrsingh/fires-api-nodejs.git
+   $ cd fires-api-nodejs
+   ```  
+3. Deploy the application without starting it
+   ```
+   $ ibmcloud cf push --no-start
+   ```
+
+## Getting Started on your local machine
+
+To run this application on your local machine, first install Node.js.
+
+1. Clone this repository
+   ```
+   $ git clone git@github.com:rajrsingh/fires-api-nodejs.git
+   $ cd fires-api-nodejs
+   ```  
+2. Install the dependencies
+   ```
+   $ npm install
+   ```
+3. Create a `vcap-local.json` file with your Bluemix app's VCAP environment variables
+
+    - When running this app locally you can get your Cloudant credentials from Bluemix (VCAP_SERVICES in "cf env" output or the Environment Variables section for an app in the Bluemix console dashboard). Once you have the credentials, paste them into a file called vcap-local.json. Alternately you could point to a local database here instead of a Bluemix service.
+    - URL will be in this format: https://username:password@xxxxxxxxx-bluemix.cloudant.com
+
+## License
+
+This code is licensed under Apache 2.0. Full license text is available in [LICENSE](https://github.com/rajrsingh/fires-api-nodejs/tree/master/LICENSE).
---
+++ app.js
@@ -0,0 +1,120 @@
+// https://firms.modaps.eosdis.nasa.gov/active_fire/
+const fs = require('fs')
+const request = require('request-promise-native')
+var cloudant = require('@cloudant/cloudant')
+const parse = require('csv-parse')
+require('should')
+
+const VIIRS_URL = 'http://localhost/call4code/fire/test.csv'
+// const VIIRS_URL = 'https://firms.modaps.eosdis.nasa.gov/data/active_fire/viirs/csv/VNP14IMGTDL_NRT_Global_24h.csv'
+var db;
+// var cloudant;
+var fileToUpload;
+var dbCredentials = {
+    dbName: 'fire_viirs_db'
+};
+
+function getDBCredentialsUrl(jsonData) {
+  var vcapServices = JSON.parse(jsonData);
+  // Pattern match to find the first instance of a Cloudant service in
+  // VCAP_SERVICES. If you know your service key, you can access the
+  // service credentials directly by using the vcapServices object.
+  for (var vcapService in vcapServices) {
+      if (vcapService.match(/cloudant/i)) {
+          return vcapServices[vcapService][0].credentials.url;
+      }
+  }
+}
+
+function initDBConnection() {
+  //When running on Bluemix, this variable will be set to a json object
+  //containing all the service credentials of all the bound services
+  if (process.env.VCAP_SERVICES) {
+      dbCredentials.url = getDBCredentialsUrl(process.env.VCAP_SERVICES);
+  } else { //When running locally, the VCAP_SERVICES will not be set
+      // When running this app locally you can get your Cloudant credentials
+      // from Bluemix (VCAP_SERVICES in "cf env" output or the Environment
+      // Variables section for an app in the Bluemix console dashboard).
+      // Once you have the credentials, paste them into a file called vcap-local.json.
+      // Alternately you could point to a local database here instead of a
+      // Bluemix service.
+      // url will be in this format: https://username:password@xxxxxxxxx-bluemix.cloudant.com
+      dbCredentials.url = getDBCredentialsUrl(fs.readFileSync("vcap-local.json", "utf-8"));
+  }
+  cloudant = require('@cloudant/cloudant')(dbCredentials.url);
+    // check if DB exists if not create
+    cloudant.db.create(dbCredentials.dbName, function(err, res) {
+      if (err) {
+          console.log('Could not create new db: ' + dbCredentials.dbName + ', it might already exist.');
+      }
+  });
+  db = cloudant.use(dbCredentials.dbName);
+}
+
+const handleFail = function (err) { // API call failed...
+  console.error(err.hasOwnProperty('message') ? err.message : err)
+}
+
+const callActiveFireData = function () {
+  request(VIIRS_URL)
+    .then(fireCSV => {
+      initDBConnection();
+
+      let csv_parser_options = {
+        cast: true, 
+        columns: true, 
+        relax_column_count: true, 
+        trim: true
+      }
+
+      parse(fireCSV, csv_parser_options, (err, firedata) => {
+        if (err) {
+          console.log("ERR: "+err)
+          return
+        }
+
+        firedata.forEach(fire => {
+          let id = fire['acq_date']+'T'+fire['acq_time']+'_'+fire['longitude']+'_'+fire['latitude']
+          let f = {
+            "type": "Feature", 
+            "geometry": {
+              "type": "Point", 
+              "coordinates": [ fire['longitude'], fire['latitude'] ]
+            }, 
+            "properties": {
+              "bright_ti4": fire['bright_ti4'], 
+              "bright_ti5": fire['bright_ti5'], 
+              "scan": fire['scan'], 
+              "track": fire['track'], 
+              "acq_date": fire['acq_date'], 
+              "acq_time": fire['acq_time'], 
+              "satellite": fire['satellite'], 
+              "confidence": fire['confidence'], 
+              "version": fire['version'], 
+              "frp": fire['frp'], 
+              "daynight": fire['daynight'], 
+            }
+          }
+
+          console.log(f)
+          db.insert(f, id, (err, body, header) => {
+            if (err) {
+              return console.log(err)
+            } else {
+              console.log("Inserted "+id)
+            }
+          })
+        });
+      })      
+    })
+    .catch(handleFail)
+}
+
+/**
+ * Here's an example of setting up a job to look for weather
+ * headlines every 10 minutes. You can set up as many of these
+ * jobs as you like and query different geographies.
+ */
+// const interval = 1000 * 60 * 60 * 24 // every 24 hours
+// let jobInterval = setInterval(() => callActiveFireData(), interval)
+callActiveFireData()
